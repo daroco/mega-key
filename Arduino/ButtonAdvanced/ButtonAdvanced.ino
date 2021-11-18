a324943b6be5a0179ae04c84dc2afa0d0f9043c1
Dave Correll <drcorrell@gmail.com>
2021-11-17 22:05:12 -0500

    Code cleanup

--- Arduino/ButtonAdvanced/ButtonAdvanced.ino
+++ Arduino/ButtonAdvanced/ButtonAdvanced.ino
@@ -10,6 +10,10 @@
 #define ROTARY_PIN 1
 #define VOLUME_UP_PIN 2
 #define VOLUME_DOWN_PIN 3
+#define NUMPIXELS 1
+#define LED_PIN 11
+#define REACTION_TIME 500
+
 
 int val;
 int encoder0Pos = 0;
@@ -17,62 +21,32 @@ int encoder0PinALast = LOW;
 int n = LOW;
 long firstPixelHueBase = 20000;
 
-#define NUMPIXELS 1
-#define LED_PIN 11
-#define REACTION_TIME 500
-
-Button2 button;
-
-//class Button {
-//    private:
-//        bool _state;
-//        uint8_t _pin;
-//
-//    public:
-//        Button(uint8_t pin) : _pin(pin) {}
-//
-//        void begin() {
-//            pinMode(_pin, INPUT_PULLUP);
-//            _state = digitalRead(_pin);
-//        }
-//
-//        bool isReleased() {
-//            bool v = digitalRead(_pin);
-//            if (v != _state) {
-//                _state = v;
-//                if (_state) {
-//                    return true;
-//                }
-//            }
-//            return false;
-//        }
-//};
-
-//Button myButton(KEY_PIN);
-Click clicker(ROTARY_PIN, PULLUP);
-//DoubleClick key(KEY_PIN, PULLUP, REACTION_TIME);
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
+Button2 KeySwitch;
+Click VolumeKnob(ROTARY_PIN, PULLUP);
+Adafruit_NeoPixel NeoPixel = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
-  //  pinMode(KEY_PIN, INPUT_PULLUP);
-  //  pinMode(ROTARY_PIN, INPUT_PULLUP);
-  pinMode (VOLUME_DOWN_PIN, INPUT_PULLUP);
-  pinMode (VOLUME_UP_PIN, INPUT_PULLUP);
 
   Consumer.begin();
   Keyboard.begin();
-  pixels.begin(); // This initializes the NeoPixel library.
-  pixels.setBrightness(100);
-  clicker.begin();
-  //  myButton.begin();
-  button.begin(KEY_PIN);
-  //  key.begin();
-  //  key.setMaxClicks(3);
-  button.setClickHandler(clickIt);
-  button.setDoubleClickHandler(doubleClick);
-  button.setTripleClickHandler(tripleClick);
-  button.setLongClickHandler(longClick);
+  NeoPixel.begin();
+  VolumeKnob.begin();
+  KeySwitch.begin(KEY_PIN);
+
+  pinMode(VOLUME_DOWN_PIN, INPUT_PULLUP);
+  pinMode(VOLUME_UP_PIN, INPUT_PULLUP);
+
+  NeoPixel.setBrightness(100);
+
+  KeySwitch.setClickHandler(clickIt);
+  KeySwitch.setDoubleClickHandler(doubleClick);
+  KeySwitch.setTripleClickHandler(tripleClick);
+  KeySwitch.setLongClickHandler(longClick);
+
+  int pixelHue = firstPixelHueBase + (65536L / NeoPixel.numPixels()) - 512;
+  NeoPixel.setPixelColor(0, 0, 128, 0);
+  NeoPixel.show();
 }
 
 void loop() {
@@ -80,59 +54,13 @@ void loop() {
   if ((encoder0PinALast == LOW) && (n == HIGH)) {
     if (digitalRead(VOLUME_DOWN_PIN) == LOW) {
       Consumer.write(MEDIA_VOLUME_DOWN );
-      int pixelHue = firstPixelHueBase + (65536L / pixels.numPixels()) - 512;
-      pixels.setPixelColor(0, pixels.gamma32(pixels.ColorHSV(pixelHue)));
-      firstPixelHueBase = pixelHue;
-      pixels.show();
     } else {
       Consumer.write(MEDIA_VOLUME_UP );
-      int pixelHue = firstPixelHueBase + (65536L / pixels.numPixels()) + 512;
-      pixels.setPixelColor(0, pixels.gamma32(pixels.ColorHSV(pixelHue)));
-      firstPixelHueBase = pixelHue;
-      pixels.show();
     }
-    Serial.print (encoder0Pos);
-    Serial.println ("/");
   }
   encoder0PinALast = n;
-  //  if (myButton.isReleased()) {
-  //    Serial.println(F("Released"));
-  //  }
 
-
-  //
-  //  switch (key.clickCount()) {
-  //    case 1:
-  //      Consumer.write(MEDIA_PLAY_PAUSE);
-  //      Serial.println(1);
-  ////      delay(REACTION_TIME);
-  ////      Keyboard.releaseAll();
-  //      break;
-  //    case 2:
-  //      Consumer.write(MEDIA_NEXT);
-  //      Serial.println(2);
-  ////      delay(REACTION_TIME);
-  ////      Keyboard.releaseAll();
-  //      break;
-  //    case 3:
-  //      Consumer.write(MEDIA_PREV);
-  //      Serial.println(3);
-  ////      delay(REACTION_TIME);
-  ////      Keyboard.releaseAll();
-  //      break;
-  //    default:
-  //      Keyboard.releaseAll();
-  //      break;
-  //  }
-
-  //  if (key.clickCount() > 0) {
-  //    Serial.println(key.clickCount());
-  //  }
-
-
-
-  if (clicker.clicked()) {
-
+  if (VolumeKnob.clicked()) {
     Serial.println("A SHORT press is detected");
     Keyboard.press(KEY_LEFT_CTRL);
     Keyboard.press(KEY_LEFT_SHIFT);
@@ -141,9 +69,8 @@ void loop() {
     Keyboard.releaseAll();
     delay(300);
   }
-  int currentState = digitalRead(KEY_PIN);
-  int rotaryState = digitalRead(ROTARY_PIN);
-  button.loop();
+
+  KeySwitch.loop();
 }
 
 
